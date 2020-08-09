2d29c765c3e97a37b7a6314c98be48b19b27ba55
Dikiy Igor <wildig1014@yandex.ru>
2020-08-10 01:00:48 +0300

    Coverage + excludeded code

---
+++ tests/test_bullet.py
@@ -0,0 +1,37 @@
+from math import pi
+from unittest import TestCase
+
+from back.effects import EffectFactory
+from back.entities import Bullet, Player
+from tests.helpers import round_list
+
+
+class Test(TestCase):
+    def setUp(self):
+        self.pl = Player(0, 0)
+        self.pl.set_effect_factory(EffectFactory())
+        self.ent = Bullet(1, 2, 90 * pi / 180, self.pl)
+
+    def test_action_on_collision_false(self):
+        ent_other = Bullet(-12, 10, 0, self.pl)
+        self.ent.next(0.02, [ent_other])
+        self.assertEqual(1, self.ent.hp)
+        self.assertEqual([[26.0, 3.0], [-9.0, 3.0]], round_list(self.ent.geometry.bounds, 0))
+
+    def test_action_on_collision_true(self):
+        ent_other = Bullet(-5, 10, 0, self.pl)
+        old_pos = self.ent.geometry.bounds[:]
+        self.ent.next(0.02, [ent_other])
+        self.assertEqual(0, self.ent.hp)
+        self.assertEqual(old_pos, self.ent.geometry.bounds)
+
+    def test_get_info(self):
+        exp_res = {'id': 1,
+                   'type': 'Bullet',
+                   'x': 1,
+                   'y': 2,
+                   'r': 1.5707963267948966,
+                   'aabb': [1.0, 2.999999999999998, 36.0, 3.0],
+                   'context_id': 'ship_00123412'}
+        res = self.ent.get_info()
+        self.assertEqual(exp_res, res)
