e97a5171f132ab0d987f3fddd8b44ed29c490256
Dikiy Igor <wildig1014@yandex.ru>
2020-08-23 00:18:03 +0300

    add test

--- tests/test_game.py
+++ tests/test_game.py
@@ -12,6 +12,7 @@ class Test(TestCase):
         self.assertEqual(16, len(self.game.entities))
 
     def test_get_info_statics(self):
+        self.game.entities.sort(key=lambda x: x.context_id)
         res = self.game.entities[0].get_info()
         self.assertEqual({'aabb': (3000, 0, 3000, 3000), 'context_id': 'frame_east', 'type': 'Statics'}, res)
 
