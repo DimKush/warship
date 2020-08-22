23c3376ebeaf731140f7ec6cc31fc1efcdd296d9
Dikiy Igor <wildig1014@yandex.ru>
2020-08-22 23:42:49 +0300

    fix test

--- tests/test_game.py
+++ tests/test_game.py
@@ -6,7 +6,12 @@ from back.game import Game
 class Test(TestCase):
     def setUp(self):
         self.game = Game()
+        self.game.load_objects()
 
     def test_init_scene(self):
-        self.game.load_objects()
         self.assertEqual(16, len(self.game.entities))
+
+    def test_get_info_statics(self):
+        res = self.game.entities[0].get_info()
+        self.assertEqual({'aabb': (3000, 0, 3000, 3000), 'context_id': 'frame_east', 'type': 'Statics'}, res)
+
