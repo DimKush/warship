c4ecfda035e25b541204823a7184639a311e5d1a
Dikiy Igor <wildig1014@yandex.ru>
2020-09-10 02:26:33 +0300

    fix

--- back/config.py
+++ back/config.py
@@ -1,6 +1,7 @@
+import os
 from os.path import join
 
-RPS = 0.02
+RPS = os.environ['RPS'] or 0.02
 AREA_WIDTH = 3000
 AREA_HEIGHT = 3000
 ENEMY_COUNT = 10
@@ -8,6 +9,6 @@ ENTITY_PATH = 'entities'
 STATICS_PATH = join(ENTITY_PATH, 'statics')
 SHIPS_PATH = join(ENTITY_PATH, 'dynamics')
 
-REDIS_HOST = 'redis-18580.c56.east-us.azure.cloud.redislabs.com'
-REDIS_PORT = 18580
-REDIS_PASSWORD = 'pisMmjzMap3vTFIccM7F3dYatXkv5ozT'
+REDIS_HOST = os.environ['REDIS_HOST']
+REDIS_PORT = os.environ['REDIS_PORT']
+REDIS_PASSWORD = os.environ['REDIS_PASSWORD']
